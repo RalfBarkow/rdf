8441395b8bdc33802e0f3ac4de4bf19008c563f2
Ralf Barkow <ralf.barkow@endress.com>
2019-05-19 19:02:12 +0200

    Update rdf_test.go

--- rdf_test.go
+++ rdf_test.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/Callidon/joseki/rdf"
 )
 
 func TestTriple(t *testing.T) {
-	got := fmt.Println(Triple())
-	want := "{<http://example.org/book/book1> <http://purl.org/dc/terms/title> \"Harry Potter and the Order of the Phoenix\"}"
+	got := Triple()
+	want := rdf.NewTriple(rdf.NewURI("http://example.org/book/book1"), rdf.NewURI("http://purl.org/dc/terms/title"), rdf.NewLiteral("Harry Potter and the Order of the Phoenix"))
 	if got != want {
 		t.Errorf("got '%s' want '%s'", got, want)
 	}
